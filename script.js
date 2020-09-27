adff16354cf9775e8d59d23cc02557c6987a63ba
Ranjith <ranju08134@gmail.com>
2020-09-27 20:05:24 +0530

    added tow more routes

--- script.js
+++ script.js
@@ -58,6 +58,39 @@ app.post('/register',(req,res) =>{
     res.send(database.users[database.users.length-1]);
 });
 
+// get user infomation based on userid '/profile:id'
+
+app.get('/profile/:id', (req,res) =>{
+    const { id } =  req.params;
+    let found = false;
+    database.users.forEach(users =>{
+        if(users.id === id){
+            found = true;
+           return res.json(users);
+        }
+    });
+    if(!found){
+        res.status(400).json('Invalid user id or user does not exist');
+    }
+});
+
+// To update rank of users '/image'
+
+app.put('/image', (req,res) =>{
+    const {  id } = req.body;
+    let found = false;
+    database.users.forEach(users =>{
+        if(users.id === id){
+            found = true;
+            users.entries++;
+            return res.json(users.entries);
+        }
+      
+    });
+    if(!found){
+        res.status(400).send('user not found or invalid user id');
+    }
+});
 // server running on port 3000
 app.listen(3000,(req,res)=>{
     console.log('server running on port:3000')

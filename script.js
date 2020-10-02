3da2397bbf02f5161ce81fe922832b52a75d3957
Ranjith <ranju08134@gmail.com>
2020-10-02 20:05:21 +0530

    Updated profile end point

--- script.js
+++ script.js
@@ -84,16 +84,19 @@ app.post('/register',(req,res) =>{
 
 app.get('/profile/:id', (req,res) =>{
     const { id } =  req.params;
-    let found = false;
-    database.users.forEach(users =>{
-        if(users.id === id){
-            found = true;
-           return res.json(users);
+    db.select('*').from('users').where({id})
+    .then(user =>{
+        if(user.length){
+            res.json(user[0]);
         }
-    });
-    if(!found){
+        else{
+            res.status(400).json('Invalid user id or user does not exist');
+        }
+       
+    })
+    .catch(err =>{
         res.status(400).json('Invalid user id or user does not exist');
-    }
+    });
 });
 
 // To update rank of users '/image'

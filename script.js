154c33c099acc574ba57a6c567f890db59f6a92d
Ranjith <ranju08134@gmail.com>
2020-10-03 16:40:22 +0530

    Updated image end point

--- script.js
+++ script.js
@@ -103,18 +103,16 @@ app.get('/profile/:id', (req,res) =>{
 
 app.put('/image', (req,res) =>{
     const {  id } = req.body;
-    let found = false;
-    database.users.forEach(users =>{
-        if(users.id === id){
-            found = true;
-            users.entries++;
-            return res.json(users.entries);
-        }
-      
-    });
-    if(!found){
-        res.status(400).send('user not found or invalid user id');
-    }
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries =>{
+        // console.log(entries);
+        res.json(entries[0]);
+    })
+    .catch(err =>{
+        res.status('400').json('unable to get entries');
+    })
 });
 
 

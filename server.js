a9fb3db377ef7dc5dccb4d2ac35c4a6238168d6f
Ranjith <ranju08134@gmail.com>
2020-10-11 22:47:44 +0530

    changing response

--- server.js
+++ server.js
@@ -44,6 +44,4 @@ app.post('/imageurl', (req,res) =>{image.handleApiCall(req,res)});
 
 
 // server running on port 3000
-app.listen(process.env.PORT || 5000,()=>{
-    console.log(`server running on port:${process.env.PORT}`);
-});
+app.listen(process.env.PORT || 3000);

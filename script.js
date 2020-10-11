e0b125f824f6dd32b3ef329754c1dc0fe2408597
Ranjith <ranju08134@gmail.com>
2020-10-11 20:27:35 +0530

    upadated

--- script.js
+++ script.js
@@ -44,6 +44,6 @@ app.post('/imageurl', (req,res) =>{image.handleApiCall(req,res)});
 
 
 // server running on port 3000
-app.listen(5000,(req,res)=>{
-    console.log('server running on port:5000')
+app.listen(process.env.PORT || 5000,(req,res)=>{
+    console.log(`server running on port:${process.env.PORT}`);
 });
